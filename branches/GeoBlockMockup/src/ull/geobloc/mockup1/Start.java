a1f528fa07577a709991943b44cf1fd50cfd5dbf
joluroga <joluroga@d2573608-ceb1-11de-bb02-e302c5db49d5>
2009-11-17 12:25:37 +0000

    Una chorrada de Roda

--- branches/GeoBlockMockup/src/ull/geobloc/mockup1/Start.java
+++ branches/GeoBlockMockup/src/ull/geobloc/mockup1/Start.java
@@ -11,7 +11,7 @@ public class Start extends Activity {
 	private Button startGPSExample;
 	private Button startGeoBlockMockup;
 	
-    /** Called when the activity is first created. */
+    /** Called when the activity is first created. MIO !!! */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
