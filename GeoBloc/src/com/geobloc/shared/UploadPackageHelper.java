81518951f280ad6fae7dc13409b6ca136532cda9
goldrunner192287 <goldrunner192287@d2573608-ceb1-11de-bb02-e302c5db49d5>
2009-12-19 02:10:02 +0000

    GeoBloc (Android Client): Seems very little, but I packed nearly all the code required for building the form.xml and sending the package. The caller of UploadPackageHelper is responsible of writing all files with the GeoBlocPackageManager except forms.xml and manifest.xml (this one will be supported later) Testing will be performed later today.
    +UploadPackageHelper supporting GBForms; not tested!!
    
    To-Do:
    - Uploading GBForms to the server.
    - Add support for the manifest file in UploadPackageHelper
    - Heavy FormsDownloader refactoring, code improvement, documentation
    - Add erase file method to GeoBlocPackageManager
    - Refactor (rename) GeoBlocPackageManager
    - Add erase behaviour to FormsDownloader
    - Erase files functionality in FormsDownloader
    - Refactor (rename) FormsDownloader to FormsManager
    - Forms version detection system pending

---
+++ GeoBloc/src/com/geobloc/shared/UploadPackageHelper.java
@@ -0,0 +1,146 @@
+/**
+ * 
+ */
+package com.geobloc.shared;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.client.HttpClient;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.geobloc.ApplicationEx;
+import com.geobloc.listeners.IStandardTaskListener;
+import com.geobloc.persistance.GeoBlocPackageManager;
+import com.geobloc.shared.Utilities.WidgetType;
+import com.geobloc.tasks.UploadPackageTask;
+import com.geobloc.xml.FormTextField;
+import com.geobloc.xml.IField;
+import com.geobloc.xml.MultiField;
+import com.geobloc.xml.TextXMLWriter;
+
+/**
+ * Class built from decoupling the task of managing the packages for sending them to the server.
+ * MISSING: manifestBuilder (& manifest.xml)
+ * 
+ * @author Dinesh Harjani (goldrunner192287@gmail.com)
+ *
+ */
+public class UploadPackageHelper implements IStandardTaskListener {
+
+	private Context callerContext;
+	private HttpClient httpClient;
+	private GeoBlocPackageManager pm;
+	private ViewGroup vg;
+	private List<WidgetType> list;
+	
+	private ProgressDialog pd;
+	
+	/**
+	 * Class constructor.
+	 * @param callerContext The context of this class' creator. Normally it's an Activity, and the keyword 
+	 * 		"this" is more than enough.
+	 * @param httpClient Since this helper handles the UploadPackageTask, it requires an httpClient.
+	 * @param pm An initialized GeoBlocPackageManager, used to make the form.xml file. NOTE: It's the caller's 
+	 * 		responsability to check if the GeoBlocPackageManager is not OK.
+	 * @param vg A ViewGroup(Layout) containing all the GeoBloc Widgets to be transformed into an xml file
+	 * @param list A list of WidgetType enums, where child i of vg (vg[i]] must be of 
+	 * 		WidgetType list[i], or an exception will be thrown.
+	 */
+	public UploadPackageHelper(Context callerContext, HttpClient httpClient, GeoBlocPackageManager pm, ViewGroup vg, List<WidgetType> list) {
+		this.callerContext = callerContext;
+		this.httpClient = httpClient;
+		this.pm = pm;
+		this.vg = vg;
+		this.list = list;
+	}
+	
+	/**
+	 * This method has the responsability of starting the chain of events which lead the user to at least try 
+	 * to upload a package. For now, it always returns true, since the sending goes through NewTextReader. 
+	 * For now. An Exception can be thrown if list[i] is not of type WidgetTyoe
+	 *
+	 */
+	public boolean packAndSend() throws Exception {
+		TextXMLWriter writer = new TextXMLWriter();
+    	String xml = writer.WriteXML(this.getFields());
+    	
+    	// add form.xml
+    	boolean xmlOk = pm.addFile(GBSharedPreferences.__DEFAULT_FORM_FILENAME__, xml);
+    	
+    	pd = ProgressDialog.show(callerContext, "Working", "Uploading package to Server...");
+		pd.setIndeterminate(false);
+		pd.setCancelable(false);
+		pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		//pd.setMax(100);
+		//pd.setProgress(0);
+		
+		UploadPackageTask task = new UploadPackageTask();
+		task.setContext(callerContext.getApplicationContext());
+		/*
+		// get httpClient from ApplicationEx
+		ApplicationEx app = (ApplicationEx)this.getApplication();
+		HttpClient httpClient = app.getHttpClient();
+		*/
+		task.setHttpClient(httpClient);
+		task.setListener(this);
+		task.execute(pm.getPackageFullpath());
+    	
+    	// for now, always return true
+		return true;
+	}
+	
+	/**
+	 * Method responsible of transforming the GeoBloc Widgets into fields which will be written into a xml file.
+	 *
+	 */
+	private List<IField> getFields() throws Exception {
+		List<IField> myFields = new ArrayList<IField>();
+		FormTextField field;
+    	MultiField fields = new MultiField("form-fields");
+		// now, for every widget in ViewGroup
+    	int childCount = vg.getChildCount();
+    	String name = "Widget not found";
+    	String value = "Widget not found";
+    	for (int i = 0; i < childCount; i++) {
+    		if ((list.get(i) == WidgetType.LABEL) || (list.get(i) == WidgetType.INT) 
+    				|| (list.get(i) == WidgetType.STRING)) {
+    			EditText et = (EditText) vg.getChildAt(i);
+    			name = et.toString();
+    			value = et.getText().toString();
+    		}
+    		if (list.get(i) == WidgetType.CHECKBOX) {
+    			CheckBox cb = (CheckBox) vg.getChildAt(i);
+    			name = cb.toString();
+    			if (cb.isChecked())
+    				value = "TRUE";
+    			else
+    				value = "FALSE";
+    		}
+    		field = new FormTextField("widget", name, value);
+    	}
+    	// add MultiField
+    	myFields.add(fields);
+		return myFields;
+	}
+
+	@Override
+	public void downloadingComplete(Object result) {
+		if (pd != null)
+			pd.dismiss();
+		
+		String res = (String) result;
+		Utilities.showTitleAndMessageDialog(callerContext, "Package Report", res);		
+	}
+
+	@Override
+	public void progressUpdate(int progress, int total) {
+		// TODO Auto-generated method stub
+		
+	}
+}
