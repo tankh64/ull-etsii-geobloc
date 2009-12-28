1e8d313d6ebb9ae6cc1bf55bde811dd2030a3220
jelcaf <jelcaf@d2573608-ceb1-11de-bb02-e302c5db49d5>
2009-12-28 13:54:17 +0000

    Created the cover of the program. Making a break before moving on to the main activity.

---
+++ trunk/GeoBloc/src/com/geobloc/Cover.java
@@ -0,0 +1,55 @@
+package com.geobloc;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+/**
+ * Cover of the program
+ * 
+ * @author Jorge Carballo (jelcaf@gmail.com)
+ *
+ */
+public class Cover extends Activity {
+	
+	final Handler handler = new Handler();
+	Timer t;
+	
+	@Override
+	public void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.cover);
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+        t = new Timer(); 
+        t.schedule(new TimerTask() { 
+        	public void run() { 
+        		handler.post(new Runnable() { 
+        			public void run() {
+        		        Intent i = new Intent (getApplicationContext(), MainMenu.class);
+        		        startActivity(i);
+        		        
+        		        finish();
+        			} 
+        		}); 
+        	} 
+        }, 3000);
+	}
+	
+	@Override
+	public void onPause () {
+		super.onPause();
+		if (t != null) {
+			t.cancel();
+		}
+	}
+}
