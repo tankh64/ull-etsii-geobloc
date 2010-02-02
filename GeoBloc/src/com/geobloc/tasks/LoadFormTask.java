259caa5af755d09652364dd18354a82479d1df1c
jelcaf <jelcaf@d2573608-ceb1-11de-bb02-e302c5db49d5>
2010-02-02 19:50:29 +0000

    Branch GeoBloc2.0 - Minimal changes

--- GeoBloc/src/com/geobloc/tasks/LoadFormTask.java
+++ GeoBloc/src/com/geobloc/tasks/LoadFormTask.java
@@ -1,8 +1,15 @@
 package com.geobloc.tasks;
 
-import com.geobloc.form.FormClass;
+import java.util.ArrayList;
 
+import com.geobloc.Cover;
+import com.geobloc.form.FormClass;
+import com.geobloc.shared.Utilities;
+
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.Toast;
 
 /**
  * Task that loads a form
@@ -12,11 +19,45 @@ import android.os.AsyncTask;
  */
 public class LoadFormTask extends AsyncTask<String, Void, FormClass>{
 
+	private Context context;
+	
+	public void setContext (Context context) {
+		this.context = context;
+	}
+	
+	/* 
+	 * Runs on GUI Thread
+	 */
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+		
+	}
+	
 	@Override
 	protected FormClass doInBackground(String... params) {
 		// TODO Auto-generated method stub
+		
+		//Utilities.showToast(context, "LoadFormTask", Toast.LENGTH_SHORT);
+		
 		return null;
 	}
+	
+	/*
+	 * Runs on UI Thread
+	 */
+	/*@Override
+	protected void onProgressUpdate(Void... params) {
+
+    }*/
+	
+	/*
+	 * Runs on UI Thread
+	 */
+	/*@Override
+	protected void onPostExecute(FormClass result) {
+
+    }*/
 
 	
 }
